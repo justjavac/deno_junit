48d72546358e2d4fabaff97021c69704f0858fab
Hulk <33158585+byteHulk@users.noreply.github.com>
2022-11-09 22:40:05 +0800

    fix: add semicolon

--- mod.ts
+++ mod.ts
@@ -1,19 +1,19 @@
-export * from './decrator.ts'
-import { data } from './decrator.ts'
+export * from "./decrator.ts";
+import { data } from "./decrator.ts";
 
 interface TestDefined {
-  desc: string
-  fn: () => void
-  skip?: boolean
-  skipReason?: string
-  params?: Array<any>
+  desc: string;
+  fn: () => void;
+  skip?: boolean;
+  skipReason?: string;
+  params?: Array<any>;
 }
 
 // deno-lint-ignore no-explicit-any
 export default function (fn: any) {
-  const instance = new fn()
-  const suite = data()[fn.name]
-  const { hook, ...tests } = suite
+  const instance = new fn();
+  const suite = data()[fn.name];
+  const { hook, ...tests } = suite;
 
   if (hook == undefined) {
     Object.values<TestDefined>(tests).forEach((x) => {
@@ -32,17 +32,16 @@ export default function (fn: any) {
           ignore: x.skip,
         })
       }
-    })
-    return
+    });
+    return;
   }
 
   Deno.test(fn.name, async (t) => {
-
-    if (hook.before) hook.before.apply(instance)
+    if (hook.before) hook.before.apply(instance);
 
     for (const x of Object.values<TestDefined>(tests)) {
-      if (hook['before.each']) hook['before.each'].apply(instance)
-
+      if (hook["before.each"]) hook["before.each"].apply(instance);
+      
       if (x.params) {
         await Promise.all(
           x.params.map(
@@ -58,9 +57,9 @@ export default function (fn: any) {
         await t.step({ name: x.desc, fn: x.fn.bind(instance), ignore: x.skip })
       }
 
-      if (hook['after.each']) hook['after.each'].apply(instance)
+      if (hook["after.each"]) hook["after.each"].apply(instance);
     }
 
-    if (hook.after) hook.after.apply(instance)
-  })
+    if (hook.after) hook.after.apply(instance);
+  });
 }
